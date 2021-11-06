9ffc0b69e5c381a5634b923500587a8f04b7dcd9
Tiger Oakes <contact@tigeroakes.com>
2021-11-06 16:43:58 -0700

    Add service worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  plugins: [
+    VitePWA({
+      manifest: {
+        name: `Where's the Beep?`,
+        short_name: `Where's the Beep`,
+        background_color: '#000000',
+        theme_color: '#ffffff',
+      },
+    }),
+  ],
   build: {
-    minify: false,
     rollupOptions: {
       output: {
         paths: {
